4c46a42a05a6a9e111e0f26f10a1faab56235b94
Muhammad Uzair <uzairkhan8472@gmail.com>
2025-09-25 15:14:13 +0500

    JavaScript Quiz in Conditions

---
+++ javaScript_quizz01/quizz.js
@@ -0,0 +1,11 @@
+
+let num = +prompt("enter number ")
+if (num % 5 == 0) {
+     console.log("number is divisible by 5");
+
+} else {
+     console.log("number is not divisible by 5");
+}
+
+// using ternary operator 
+num % 5 == 0 ? console.log("your number divisible") : console.log("your number is not divisible")
